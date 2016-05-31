1c692a7106f3017a6db82fb49d09464ed27ab206
Aditya Raghuwanshi <adi.version1@gmail.com>
2016-05-31 16:27:23 -0700

    add logo

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -13,6 +13,8 @@ Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
+  config.admin_interface_logo = "logo/wishbone-chief-logo-200.png"
+  config.logo = "logo/wishbone-chief-logo-200.png"
 end
 
 Spree.user_class = "Spree::User"
